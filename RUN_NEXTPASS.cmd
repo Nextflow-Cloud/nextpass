ab20ab56802e03fddebdb2a4be4714b036da13e0
Queryzi <system@nextflow.cloud>
2022-02-20 21:28:50 -0500

    chore: remove stray comments

--- RUN_NEXTPASS.cmd
+++ RUN_NEXTPASS.cmd
@@ -1,6 +1,3 @@
 @echo off
-REM :startapp
-@REM start "NextPass Password Manager" "cmd /c python main.py && pause"
-start "Reminder" "cmd /c echo Reminder && pause >nul"
-REM goto startapp
+start "NextPass Password Manager" "cmd /c python main.py && pause"
 @echo on
